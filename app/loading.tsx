c07c66821bafbab64ab050751937b76f3481df7b
Dominic Brian <dominic91960@gmail.com>
2025-05-01 16:57:16 +0530

    feat: dummy loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <main className="bg-secondary flex h-dvh items-center justify-center">
+      <p>Loading...</p>
+    </main>
+  );
+};
+
+export default loading;
